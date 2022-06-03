e7af1d5e6f6e0cc6807a02dad9152857495c51d0
Kokurcho <kokurindmitry911@gmail.com>
2022-06-03 15:49:06 +0300

    fix: fixed htmls

--- static/libs/add_comment.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $name = $_POST['name'];
-  $comment = $_POST['comment'];
-  $mysqli = new Mysqli('localhost', 'root', '', 'comments');
-  $mysqli->query("SET NAMES utf8");
-  $mysqli->query("INSERT INTO `comm`(`name`, `comment`) VALUES('$name', '$comment')");
-  
-?>
