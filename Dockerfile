16c75971f977bb407741959621b822871d5a674b
Kokurcho <kokurindmitry911@gmail.com>
2022-06-03 20:13:56 +0300

    fix: fixed htmls

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.8
+ENV PYTHONUNBUFFERED 1
+RUN mkdir /web_django
+WORKDIR /web_django
+COPY requirements.txt /web_django/
+RUN pip install -upgrade pip && pip install -r requirements.txt
+ADD . /web_django/
--- kokurcho_site/settings.py
+++ kokurcho_site/settings.py
@@ -67,7 +67,11 @@ WSGI_APPLICATION = 'kokurcho_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
